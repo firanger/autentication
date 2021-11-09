b39979a2ac98c1212e63b2375b87801489b22ab4
firanger <jvtjuan15@gmail.com>
2021-11-08 23:19:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-avg4mj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-avg4mj)
---
+++ auth/local-auth.guard.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {}
+@UseGuards(LocalAuthGuard)
+@Post('auth/login')
+async login(@Request() req) {
+  return req.user;
+}
